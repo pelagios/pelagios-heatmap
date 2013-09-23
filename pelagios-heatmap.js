aa35985aeb44ed3dab9853366f18735e4458e2cd
Rainer Simon <rainer.simon@ait.ac.at>
2013-09-23 09:56:59 +0200

    Refactoring

---
+++ pelagios-heatmap.js
@@ -0,0 +1,24 @@
+// Namespace
+var pelagios = { }
+
+/**
+ * The pelagios heatmap
+ * @constructor
+ */
+pelagios.Heatmap = function(map, data) {
+  var heatmapLayer = L.TileLayer.heatMap({
+      radius: 12,
+      opacity: 0.8,
+      gradient: {
+          0.45: "rgb(0,0,255)",
+          0.55: "rgb(0,255,255)",
+          0.65: "rgb(0,255,0)",
+          0.95: "yellow",
+          1.0: "rgb(255,0,0)"
+      }
+  });
+  
+  heatmapLayer.addData(testData.data);
+         
+  map.addLayer(heatmapLayer);
+}
